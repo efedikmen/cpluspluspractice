8704b7a1279e752ae0cddb5b8370e562c9784483
efedikmen <efedikmen9@gmail.com>
2022-09-14 10:44:15 -0700

    9/14

--- MonteCarlo/MonteCarloPricer.cpp
+++ MonteCarlo/MonteCarloPricer.cpp
@@ -1,5 +1,6 @@
 #include "MonteCarloPricer.h"
 #include <cmath>
+#include "RandomGenerator.h"
 
 MonteCarloPricer::MonteCarloPricer()
 {
@@ -37,31 +38,72 @@ double MonteCarloPricer::price(const BlackScholes &bsm, PathDependentOption &u) 
     return exp(-bsm.GetRate()*(T-bsm.GetTime()))*res/Npaths;
 
 };
-
 double MonteCarloPricer::delta(BlackScholes &bsm, PathIndependentOption &c)
 {
     double S = bsm.GetStock();
-    double h = pow(10, -4);
-    BlackScholes bsm1(S+h, 0, bsm.GetVol(), bsm.GetRate(), bsm.GetTime());
-    BlackScholes bsm2(S-h, 0, bsm.GetVol(), bsm.GetRate(), bsm.GetTime());
+    double h = 1e-4;
+    double t = bsm.GetTime();
+    double vol = bsm.GetVol();
+    double r = bsm.GetRate();
     double delta = 0.0;
-    for (int i=0; i<Npaths; i++)
+    double dt = (c.GetMaturity()-t)/Nsteps;
+    double alpha = (r-vol*vol/2)*dt;
+    double b = vol*sqrt(dt);
+    double dlnS, lnSp, lnSm, currentlogSp, currentlogSm;
+    std::vector<double> path1(Nsteps+1), path2(Nsteps+1);
+    for (int j=0; j<Npaths; j++)
     {
-        delta += price(bsm1, c)-price(bsm2, c);
+        path1[0] = S+h;
+        path2[0] = S-h;
+        std::vector<double> normal=randn(Nsteps);
+        currentlogSp = log(S+h);
+        currentlogSm = log(S-h);
+        for (int i=0; i<=Nsteps; i++)
+        {
+            dlnS = alpha + b*normal[i];
+            lnSp = dlnS + currentlogSp;
+            lnSm = dlnS + currentlogSm;
+            path1[i+1] = exp(lnSp);
+            path2[i+1] = exp(lnSm);
+            currentlogSp = lnSp;
+            currentlogSm = lnSm;
+        }
+        delta += c.payoff(path1.back()) - c.payoff(path2.back());
     }
-    return delta/(2*h*Npaths);
+    return delta/(Npaths*2*h);
 };
 
 double MonteCarloPricer::delta(BlackScholes &bsm, PathDependentOption &c)
 {
     double S = bsm.GetStock();
-    double h = pow(10, -4);
-    BlackScholes bsm1(S+h, 0, bsm.GetVol(), bsm.GetRate(), bsm.GetTime());
-    BlackScholes bsm2(S-h, 0, bsm.GetVol(), bsm.GetRate(), bsm.GetTime());
+    double h = 1e-4;
+    double t = bsm.GetTime();
+    double vol = bsm.GetVol();
+    double r = bsm.GetRate();
     double delta = 0.0;
-    for (int i=0; i<Npaths; i++)
+    double dt = (c.GetMaturity()-t)/Nsteps;
+    double alpha = (r-vol*vol/2)*dt;
+    double b = vol*sqrt(dt);
+    double dlnS, lnSp, lnSm;
+    for (int j=0; j<Npaths; j++)
     {
-        delta += price(bsm1, c)-price(bsm2, c);
+        std::vector<double> path1(Nsteps+1), path2(Nsteps+1);
+        path1[0] = S+h;
+        path2[0] = S-h;
+        std::vector<double> normal=randn(Nsteps);
+        double currentlogSp = log(S+h);
+        double currentlogSm = log(S-h);
+        for (int i=0; i<Nsteps; i++)
+        {
+            dlnS = alpha + b*normal[i];
+            lnSp = dlnS + currentlogSp;
+            lnSm = dlnS + currentlogSm;
+            path1[i+1] = exp(lnSp);
+            path2[i+1] = exp(lnSm);
+            currentlogSp = lnSp;
+            currentlogSm = lnSm;
+        }
+        delta += c.payoff(path1) - c.payoff(path2);
     }
     return delta/(2*h*Npaths);
 };
--- MonteCarlo/main.cpp
+++ MonteCarlo/main.cpp
@@ -11,23 +11,23 @@
 
 int main()
 {   
-    CallOption c(2, 109);
-    BlackScholes bsm(107,0, 0.15, 0.05, 0);
+    CallOption c(1, 100);
+    BlackScholes bsm(100,0, 0.15, 0.05, 0);
     MonteCarloPricer m;
     std::cout << "Pricing call with analytic formula: " << c.price(bsm) << std::endl;
     std::cout << "Pricing call with Monte Carlo: " << m.price(bsm, c) << "| Delta:" << m.delta(bsm, c) << std::endl;
-    PutOption p(2, 109);
+    PutOption p(1, 100);
     std::cout << "Pricing put with analytic formula: " << p.price(bsm) << std::endl;
     std::cout << "Pricing put with Monte Carlo: " << m.price(bsm, p) << "| Delta:" << m.delta(bsm, p)  << std::endl;
-    DigitalCallOption dc(2, 109);
-    std::cout << "Pricing digital call with Monte Carlo: " << m.price(bsm, dc) << "| Delta:" << m.delta(bsm, dc) << std::endl;
-    DigitalPutOption dp(2, 109);
-    std::cout << "Pricing digital put with Monte Carlo: " << m.price(bsm, dp) << "| Delta:" << m.delta(bsm, dp) << std::endl;
-    UpAndOutOption uo(2, 109, 200);
+    DigitalCallOption dc(1, 100);
+    std::cout << "Pricing digital call with Monte Carlo: " << m.price(bsm, dc) << std::endl;
+    DigitalPutOption dp(1, 100);
+    std::cout << "Pricing digital put with Monte Carlo: " << m.price(bsm, dp) << std::endl;
+    UpAndOutOption uo(1, 100, 200);
     std::cout << "Pricing up and out option with Monte Carlo: " << m.price(bsm, uo) << "| Delta:" << m.delta(bsm, uo)  << std::endl;
-    ArithmeticAsianCall aac(2, 109);
+    ArithmeticAsianCall aac(1, 100);
     std::cout << "Pricing Arithmetic Asian Call option with Monte Carlo: " << m.price(bsm, aac) << "| Delta:" << m.delta(bsm, aac) << std::endl;
-    ArithmeticAsianPut aap(2, 109);
+    ArithmeticAsianPut aap(1, 100);
     std::cout << "Pricing Arithmetic Asian Put option with Monte Carlo: " << m.price(bsm, aap) << "| Delta:" << m.delta(bsm, aap) << std::endl;
     return 0;
 }
